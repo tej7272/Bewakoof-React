c78629a7d8cce7117e2287af3127d8eac3a9a100
tej7272 <tej7272@gmail.com>
2023-10-26 16:58:19 +0530

    update new code

--- src/components/pages/cards/BestSeller.jsx
+++ src/components/pages/cards/BestSeller.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-
 import { useGetProductsQuery } from '../../../services/productApi';
-
 import Card from './Card';
 
 const BestSeller = () => {
